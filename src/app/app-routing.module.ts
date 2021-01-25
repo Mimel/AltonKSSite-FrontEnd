c637e60e61bb89111c4cf76c9f5e82597ce6b84d
mimel <imel.matt.r@gmail.com>
2021-01-25 00:00:35 -0600

    Added routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommunityComponent } from './components/community/community.component';
+import { EventsComponent } from './components/events/events.component';
 import { HomeComponent } from './components/home/home.component';
+import { NewsComponent } from './components/news/news.component';
+import { OrganizationsComponent } from './components/organizations/organizations.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent },];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'community', component: CommunityComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'organizations', component: OrganizationsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
